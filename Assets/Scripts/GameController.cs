5137a19f941bee7eb35128b8cab87165589ee7ec
athlonneo <athlonneox2@gmail.com>
2020-03-26 23:45:52 +0700

    init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Slingshooter SlingShooter;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+
+    private bool _isGameEnded = false;
+
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        SlingShooter.InitiateBird(Birds[0]);
+    }
+
+
+    public void ChangeBird()
+    {
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+            SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
+    }
+}
